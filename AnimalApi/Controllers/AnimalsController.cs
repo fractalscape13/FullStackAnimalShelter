bcc54d5a0a72d56c57fa9519537fc71431b17c41
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-03 09:04:32 -0700

    create controllers for Animals and Users

---
+++ AnimalApi/Controllers/AnimalsController.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using AnimalApi.Models;
+
+namespace AnimalApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  [ApiVersion("1.0")]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly AnimalApiContext _db;
+
+    public AnimalsController(AnimalApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get(string name, string type, string breed)
+    {
+      var query = _db.Animals.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (type != null)
+      {
+        query = query.Where(entry => entry.Type == type);
+      }
+
+      if (breed != null)
+      {
+        query = query.Where(entry => entry.Breed == breed);
+      }
+
+      List<Animal> animalList = query.ToList(); 
+      animalList.Sort();
+      return animalList;
+    }
+
+    // GET api/animals/5
+    [HttpGet("{id}")]
+    public ActionResult<Animal> Get(int id)
+    {
+      return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+    }
+
+    // POST api/animals
+    // [Authorize]
+    [HttpPost]
+    public void Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+    }
+
+    // PUT api/animals/1
+    // [Authorize]
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Animal animal)
+    {
+      animal.AnimalId = id;
+      _db.Entry(animal).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/animals/1
+    // [Authorize]
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+      _db.Animals.Remove(animalToDelete);
+      _db.SaveChanges();
+    }
+  }
+}
---
+++ AnimalApi/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using AnimalApi.Services;
+using AnimalApi.Models;
+using System.Linq;
+
+namespace AnimalApi.Controllers
+{
+  // [Authorize]
+  [ApiController]
+  [Route("api/[controller]")]
+  [ApiVersion("1.0")]
+  public class UsersController : ControllerBase
+  {
+    private IUserService _userService;
+    private readonly AnimalApiContext _db;
+
+    public UsersController(AnimalApiContext db, IUserService userService)
+    {
+      _db = db;
+      _userService = userService;
+    }
+
+    // POST /users
+    // [AllowAnonymous]
+    [HttpPost]
+    public void Register([FromBody]User newUser)
+    {
+      _db.Users.Add(newUser);
+      _db.SaveChanges();
+    }
+
+    // POST /users/authenticate
+    // [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate([FromBody]User userParam)
+    {
+      var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+      if (user == null)
+      {
+        return BadRequest(new { message = "Username or password is incorrect" });
+      }
+      else
+      {
+        return Ok(user);
+      }
+    }
+    
+    // GET /users
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+      var users = _userService.GetAll();
+      return Ok(users);
+    }
+  }
+}
