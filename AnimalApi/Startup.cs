e866afd791f16cd9a9963a4717df9620b1f4bf2d
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-03 15:43:38 -0700

    remove comments, fix spacing

--- AnimalApi/Startup.cs
+++ AnimalApi/Startup.cs
@@ -43,13 +43,12 @@ namespace AnimalApi
 			var appSettingsSection = Configuration.GetSection("AppSettings");
 			services.Configure<AppSettings>(appSettingsSection);
 
-			// jwt authentication
 			var appSettings = appSettingsSection.Get<AppSettings>();
 			var key = Encoding.ASCII.GetBytes(appSettings.Secret);
 			services.AddAuthentication(x =>
 			{
 				x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
       })
 			.AddJwtBearer(x =>
 			{
@@ -64,11 +63,9 @@ namespace AnimalApi
 				};
 			});
 
-			// configure DI for application services
 			services.AddScoped<IUserService, UserService>();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -77,10 +74,8 @@ namespace AnimalApi
 			}
 			else
 			{
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
-			// global cors policy
 			app.UseCors(x => x
 				.AllowAnyOrigin()
 				.AllowAnyMethod()
