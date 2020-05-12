4968a49a8ce2396fe20eee10dfae19443bb06780
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-12 10:17:07 -0700

    add form, add success/failure action types for get request
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>

--- frontend/src/Actions/ActionTypes.js
+++ frontend/src/Actions/ActionTypes.js
@@ -1,7 +1,7 @@
 export const GET_ANIMALS = 'GET_ANIMALS';
 export const GET_ANIMALS_FAILURE='GET_ANIMALS_FAILURE'
 export const GET_ANIMALS_SUCCESS='GET_ANIMALS_SUCCESS'
+export const NEW_ANIMAL = 'NEW_ANIMAL';
 export const DELETE_ANIMAL = 'DELETE_ANIMAL';
 export const EDIT_ANIMAL = 'EDIT_ANIMAL';
-export const NEW_ANIMAL = 'NEW_ANIMAL';
 export const SHOW_FORM = 'SHOW_FORM';
